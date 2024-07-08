58139c3bfb9319e62fa0ba96d675560f8534823c
Joserafa98 <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-07-08 18:32:28 +0000

    Creando generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function ExcuseGenerator() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while"
+    ];
+
+    let whoIndex = Mat.floor(Math.random(-1) * who.length);
+    let actionIndex = Mat.floor(Math.random(-1) * action.length);
+    let whatIndex = Mat.floor(Math.random(-1) * what.length);
+    let whenIndex = Mat.floor(Math.random(-1) * when.length);
+
+    let ExcuseGenerator =
+      who[whoIndex] + action[actionIndex] + what[whatIndex] + when[whenIndex];
+    return ExcuseGenerator;
+  }
   console.log("Hello Rigo from the console!");
 };
